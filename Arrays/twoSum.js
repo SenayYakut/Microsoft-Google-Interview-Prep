6b80b0648558db3e759b42933d15bff9d997f1a7
Senay Yakut <senaykt@gmail.com>
2019-10-18 20:40:07 -0700

    Google Prep

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -9,3 +9,18 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
+const twoSum=function(arr, target){
+    let newAr1=[];
+    
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+        if(arr[i] + arr[j] === target){
+            newAr1.push(arr[i],arr[j]);
+        }else{
+            return false;
+        }
+        return newAr1;
+    }
+}
+
+console.log(twoSum([1,2,3,4,5,6], 7));

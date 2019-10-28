c590b3a340d83791b5d2c09da7000dea7bf5053e
Senay Yakut <senaykt@gmail.com>
2019-10-28 10:34:33 -0700

    another brute force solution but consoles all of the arrays which has target sum

--- Arrays/3Sum.js
+++ Arrays/3Sum.js
@@ -28,3 +28,19 @@ var treeSum_Brute=function(nums){
     return uniqueTriplets;
 }
 console.log(treeSum_Brute([-1,0,1,2,-1,-4]));
+
+// another brute force solution but it consoles all of the arrays, not just the unique ones  
+function sumOf(arr){
+    var newArr=[];
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            for(let z=j+1; z<arr.length; z++){
+                if(arr[i]+arr[j]+arr[z]===0){
+                    newArr.push([arr[i],arr[j],arr[z]]);
+                }
+            }
+        }
+    }
+    return newArr;
+}
+console.log(sumOf([-1,0,1,2,-1,-4]));

e266d4f4148481ca19c033d44af29b94d1405f12
Senay Yakut <senaykt@gmail.com>
2019-10-30 10:38:32 -0700

    my brute force solution for insert position challange question

--- Arrays/searchInsertPosition.js
+++ Arrays/searchInsertPosition.js
@@ -1,16 +1,19 @@
 /*Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
 You may assume no duplicates in the array.*/
-const searchInsertPosition=function(nums, target){
-    for(let i=0; i<nums.length; i++){
-        if(nums[i]===target){
-            return i;
-        }else{
-            while(nums[i]<taget){
-                i++;
-            }
+//My brute forse solution this is O(n^2);
+//not very oprimal
+//lets think two pointer solution
+var position=function(arr,target){
+    for(let i=0; i<arr.length; i++){
+      if(arr[i]===target){
+        return i;
+      }else{
+        while(arr[i]<target){
+          i++;
         }
         return i;
-
-}
-console.log(searchInsertPosition([1,3,5,6], 5));
+      }
+    }
+  };
+  console.log(position([1,3,5,6], 7));

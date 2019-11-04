2435494e37d05e2993a310e07ac6a8ce3a8c66cd
Senay Yakut <senaykt@gmail.com>
2019-11-04 13:21:52 -0800

    pascal's triangle

---
+++ Arrays/pascalsTriangle.js
@@ -0,0 +1,28 @@
+    
+var generate = function(numRows) {
+    // we need the base and it will be able to build up
+    let arr = [[1], [1, 1]];
+    // making a helper function to create the next row
+    function nextRow(nums) {
+        // the first number is always 1 
+        let newArr = [1];
+        
+        for (let i = 0; i < nums.length - 1; i++) {
+            newArr.push(nums[i]+nums[i+1]);
+        };
+        // the last number is always 1 
+        newArr.push(1);
+        return newArr;
+    };
+    // now we can build up our Pascal's Triangle
+    for (let i = 1; i < numRows - 1; i++) {
+        arr.push(nextRow(arr[i]));
+    }
+    
+    // we can't just return arr, 
+    //there are some edge cases that'll create the errors
+    // if the inputs are 0 and 1 it would retrun [[1], [1, 1]] 
+    // slicing the arr will solve the edge cases 
+    return arr.slice(0, numRows);
+};
+console.log(generate(5));
---
+++ Arrays/plusOne.js
@@ -0,0 +1,8 @@
+function plusOne(arr){
+    arr=arr.splice("").join()+1;
+    newArr=arr+1;
+    newArr=newArr.splice(",");
+    return newArr;
+    
+}
+console.log(newArr([1,2,3]));

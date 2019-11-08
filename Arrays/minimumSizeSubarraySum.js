963677201931ce740cb4db8457b6ef564c10d9fa
Senay Yakut <senaykt@gmail.com>
2019-11-07 18:32:22 -0800

    sliding window

--- Arrays/minimumSizeSubarraySum.js
+++ Arrays/minimumSizeSubarraySum.js
@@ -2,3 +2,36 @@
 
 //Example: 
 
+var minSubArrayLen = function(s, nums) {
+    let sum = 0;
+    let count = 0;
+    let countArr = [];
+    
+    for(let i = 0; i < nums.length; i++) {
+      sum += nums[i];
+      count++;
+      
+      if(sum >= s) {
+        return count;
+      } 
+      
+        for(let j = i+1; j < nums.length; j++) {
+          sum += nums[j];
+          count++;
+          
+          if(sum >= s) {
+            countArr.push(count);
+           break;
+          }
+        }
+        sum = 0;
+        count = 0;
+    }
+    
+    if (countArr.length === 0) {
+      return 0;
+    }
+    return Math.min(...countArr)
+};
+
+minSubArrayLen(15, [5,1,3,5,10,7,4,9,2,8])

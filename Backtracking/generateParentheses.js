70ae320d640b61a1482977bef0969efceb19d280
Senay Yakut <senaykt@gmail.com>
2019-11-18 16:12:19 -0800

    we will need to print all possible

--- Backtracking/generateParentheses.js
+++ Backtracking/generateParentheses.js
@@ -1,1 +1,38 @@
 //Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//this function will only look if it is balanced
+function(str) {
+    var rightParentheses = '(';
+    var leftParentheses = ')';
+    var rightCount = 0;
+    var leftCount = 0;
+
+    for (var i = 0; i <= str.length; i++) {
+       if (rightParentheses == str.charAt(i))
+          rightCount++;
+       else if (leftParentheses == str.charAt(i))
+          leftCount++;
+
+       if (rightCount < leftCount)
+          return false;
+    }
+
+    return rightCount == leftCount;
+}
+//this function will print all of the possible strings
+function nPair(n) {
+    if (n == 0)
+        return [""];
+
+    var result = [];
+    for (var i = 0; i < n; ++i) {
+
+        var lefts = nPair(i);
+        var rights = nPair(n - i - 1);
+
+        for (var l = 0; l < lefts.length; ++l)
+            for (var r = 0; r < rights.length; ++r)
+                result.push("(" + lefts[l] + ")" + rights[r]);
+    }
+
+    return result;
+}

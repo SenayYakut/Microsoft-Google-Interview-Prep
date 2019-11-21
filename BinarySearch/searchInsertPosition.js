8b839ccc2677ba9647bf02206482af1f0fd97e82
Senay Yakut <senaykt@gmail.com>
2019-11-21 14:12:36 -0800

    Binary Search is always O(Log n)

--- BinarySearch/searchInsertPosition.js
+++ BinarySearch/searchInsertPosition.js
@@ -1,26 +1,24 @@
-var searchIndex=function(arr, target){
+//Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//This is the binary search solution and it is only good for sorted arrays. we will assume that array is sorted.
+//it is O(log n);
+var searchPosition=function(arr, target){
     let start=0;
     let end=arr.length-1;
     let index=Math.floor((start+end)/2);
     if(target>arr[end]){
         index=arr.length;
     }
-    while(start<end){
-        let value=arr[index];
-        if(target===value){
-            return index;
-            break;    
-        }else if(target<value){
-            end=index;
-        }else{
-            start=index+1;
+        while(start<end){
+            let value=arr[index];
+            if(target<value){
+                end=index;
+            }else{
+                start=index+1;
+            }
+            index=Math.floor((start+end)/2);
         }
-        index=Math.floor((start+end)/2);   
-      }
-      return index;
-    }
-    console.log(searchIndex([1,3,5,6],1));
+    return index;
+}
+console.log(searchPosition([1,2,3,4,5],8));//some edge cases
 
 
-
-    

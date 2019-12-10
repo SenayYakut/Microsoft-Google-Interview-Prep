7b5060ef31b6e423966efcfe4c84ba2deec1fb8a
Senay Yakut <senaykt@gmail.com>
2019-12-09 16:24:07 -0800

    solution is in progress

--- Breadth-FirstSearch/numberOfIslands.js
+++ Breadth-FirstSearch/numberOfIslands.js
@@ -1,1 +1,37 @@
 //Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+let numIslands = function(grid) {
+
+    let markIsland = function(grid, x, y, visited) {
+      if(x < 0 || x > grid.length - 1 || y < 0 || y > grid[x].length - 1) {
+        return;
+      }
+      if(visited[x][y] === true) {
+        return;
+      }
+      visited[x][y] = true;
+      if(grid[x][y] === '0') {
+        return;
+      }
+      markIsland(grid, x - 1, y, visited);
+      markIsland(grid, x + 1, y, visited);
+      markIsland(grid, x, y - 1, visited);
+      markIsland(grid, x, y + 1, visited);
+    };
+    
+      let visited = [];
+      for(let i = 0; i < grid.length; i++) {
+        visited[i] = [];
+      }
+      let count = 0;
+      for(let x = 0; x < grid.length; x++) {
+        for(let y = 0; y < grid[x].length; y++) {
+          if(!visited[x][y] && grid[x][y] === '1') {
+            count++;
+            markIsland(grid, x, y, visited);
+          }
+          visited[x][y] = true;
+        }
+      }
+      return count;
+    };
+    

143e6386dc6eeec69a32e3f89873cf92af701fc9
Senay Yakut <senaykt@gmail.com>
2019-12-10 11:32:49 -0800

    new approach

--- Breadth-FirstSearch/numberOfIslands.js
+++ Breadth-FirstSearch/numberOfIslands.js
@@ -1,37 +1,28 @@
 //Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
-let numIslands = function(grid) {
+var n;
+var m;
 
-    let markIsland = function(grid, x, y, visited) {
-      if(x < 0 || x > grid.length - 1 || y < 0 || y > grid[x].length - 1) {
-        return;
-      }
-      if(visited[x][y] === true) {
-        return;
-      }
-      visited[x][y] = true;
-      if(grid[x][y] === '0') {
-        return;
-      }
-      markIsland(grid, x - 1, y, visited);
-      markIsland(grid, x + 1, y, visited);
-      markIsland(grid, x, y - 1, visited);
-      markIsland(grid, x, y + 1, visited);
-    };
-    
-      let visited = [];
-      for(let i = 0; i < grid.length; i++) {
-        visited[i] = [];
-      }
-      let count = 0;
-      for(let x = 0; x < grid.length; x++) {
-        for(let y = 0; y < grid[x].length; y++) {
-          if(!visited[x][y] && grid[x][y] === '1') {
-            count++;
-            markIsland(grid, x, y, visited);
-          }
-          visited[x][y] = true;
-        }
-      }
-      return count;
-    };
-    
+var numIslands = function(grid) {
+	var count = 0;
+	n = grid.length;
+	if (n == 0) return 0;
+	m = grid[0].length;
+
+	for (var i = 0; i < n; i++) {
+		for (var j = 0; j < m; j++)
+			if (grid[i][j] == '1') {
+				DFS(grid, i, j);
+				++count;
+			}
+	}
+	return count;
+};
+
+function DFS(grid, i, j) {
+	if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+	grid[i][j] = '0';
+	DFS(grid, i + 1, j);
+	DFS(grid, i - 1, j);
+	DFS(grid, i, j + 1);
+	DFS(grid, i, j - 1);
+}
